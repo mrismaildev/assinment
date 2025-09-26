f2b5d69bce676fe2b8ba1c77d66fb5ea9edd33e4
mrismaildev <ismail.dev.khulna@gmail.com>
2025-09-26 08:01:57 +0600

    ddf

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
 
-]
+var containerEl = document.querySelector('.filtergal');
+var mixer = mixitup(containerEl);
+
+$('.slickService').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  prevArrow: '<i class="fa-solid fa-angle-left prev"></i>',
+  nextArrow: '<i class="fa-solid fa-angle-right next"></i>',
+});
+
+$('.serviceSlick').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  prevArrow: '<i class="fa-solid fa-arrow-left prevSeond"></i>',
+  nextArrow: '<i class="fa-solid fa-arrow-right nextSecond"></i>',
+});
+
+$('.serviceSlickThree').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  prevArrow: '<i class="fa-solid fa-arrow-left prevSeond"></i>',
+  nextArrow: '<i class="fa-solid fa-arrow-right nextSecond"></i>',
+});
